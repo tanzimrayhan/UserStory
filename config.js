4032133c236ce0afba42e263a36e01fe2a14ce99
Tanzim Ali <tanzim_rayhan@yahoo.com>
2019-02-19 00:52:38 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports={
+    "database":"mongodb://admin:tmobile1@ds151293.mlab.com:51293/userstory",
+    "port":process.env.PORT || 3000,
+    "secretKey":"admin"
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var morgan=require("morgan")
+var config=require('./config')
+var mongoose=require('mongoose')
+
+mongoose.connect(config.database, function(err){
+    if(err){
+        console.log(err);
+    }
+    else console.log("Connected to DB.")
+})
+var app=express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+
+var api=require('./app/routes/api')(app,express);
+
+app.use('/api',api)
+
+
+app.get('*',function(req, res){
+    res.sendFile(__dirname+'/index.html')
+})
+
+app.listen(config.port, function(err){
+    if(err){
+        console.log(err);
+    }
+    else console.log("Listensing on port 3000");
+
+})
